02546e74563daa157f21e125922bd43e3b7d54b6
Snehashis Dalui <snehashisdalui087@gmail.com>
2026-09-01 10:19:40 +0530

    Convert PHP script to HTML form for counting 'aa'

--- Assignment_2/5.php
+++ Assignment_2/5.php
@@ -1,15 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Count 'aa'</title>
+</head>
+<body>
 
-$str = readline("Enter a string: ");
+    <h2>Count Occurrences of "aa"</h2>
 
-$count = 0;
+    <form method="post">
+        <label>Enter a String:</label>
+        <input type="text" name="str" required>
+        <br><br>
 
-for ($i = 0; $i < strlen($str) - 1; $i++) {
-    if ($str[$i] == 'a' && $str[$i + 1] == 'a') {
-        $count++;
+        <button type="submit">Count</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $str = $_POST["str"];
+        $count = 0;
+
+        for ($i = 0; $i < strlen($str) - 1; $i++) {
+            if ($str[$i] == 'a' && $str[$i + 1] == 'a') {
+                $count++;
+            }
+        }
+
+        echo "<h3>Number of 'aa': " . $count . "</h3>";
     }
-}
+    ?>
 
-echo "Number of 'aa': " . $count;
-
-?>
+</body>
+</html>
