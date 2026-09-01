9e9a873703f5d6fe7d9056e6ad299de23d782afa
Snehashis Dalui <snehashisdalui087@gmail.com>
2026-09-01 10:20:21 +0530

    Refactor to HTML form for FizzBuzz string check
    Converted the script to an HTML form that accepts user input and displays results based on the FizzBuzz logic.

--- Assignment_2/7.php
+++ Assignment_2/7.php
@@ -1,18 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FizzBuzz String</title>
+</head>
+<body>
 
-$str = readline("Enter a string: ");
+    <h2>FizzBuzz String Checker</h2>
 
-$startsWithF = substr($str, 0, 1) == "F";
-$endsWithB = substr($str, -1) == "B";
+    <form method="post">
+        <label>Enter a String:</label>
+        <input type="text" name="str" required>
+        <br><br>
 
-if ($startsWithF && $endsWithB) {
-    echo "FizzBuzz";
-} elseif ($startsWithF) {
-    echo "Fizz";
-} elseif ($endsWithB) {
-    echo "Buzz";
-} else {
-    echo $str;
-}
+        <button type="submit">Check</button>
+    </form>
 
-?>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $str = $_POST["str"];
+
+        $startsWithF = substr($str, 0, 1) == "F";
+        $endsWithB = substr($str, -1) == "B";
+
+        if ($startsWithF && $endsWithB) {
+            echo "<h3>FizzBuzz</h3>";
+        } elseif ($startsWithF) {
+            echo "<h3>Fizz</h3>";
+        } elseif ($endsWithB) {
+            echo "<h3>Buzz</h3>";
+        } else {
+            echo "<h3>" . htmlspecialchars($str) . "</h3>";
+        }
+    }
+    ?>
+
+</body>
+</html>
