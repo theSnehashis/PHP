6678b84f8c5b94a7e66598d593b099bf965a6133
Snehashis Dalui <snehashisdalui087@gmail.com>
2026-09-01 10:20:00 +0530

    Convert CLI number search to HTML form

--- Assignment_2/6.php
+++ Assignment_2/6.php
@@ -1,14 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Number in Array</title>
+</head>
+<body>
 
-$input = readline("Enter array elements separated by spaces: ");
-$array = explode(" ", $input);
+    <h2>Search Number in Array</h2>
 
-$num = readline("Enter the number to search: ");
+    <form method="post">
+        <label>Enter array elements separated by spaces:</label>
+        <input type="text" name="input" required>
+        <br><br>
 
-if (in_array($num, $array)) {
-    echo "Number is present in the array.";
-} else {
-    echo "Number is not present in the array.";
-}
+        <label>Enter the number to search:</label>
+        <input type="number" name="num" required>
+        <br><br>
 
-?>
+        <button type="submit">Search</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $input = $_POST["input"];
+        $array = explode(" ", $input);
+
+        $num = $_POST["num"];
+
+        if (in_array($num, $array)) {
+            echo "<h3>Number is present in the array.</h3>";
+        } else {
+            echo "<h3>Number is not present in the array.</h3>";
+        }
+    }
+    ?>
+
+</body>
+</html>
