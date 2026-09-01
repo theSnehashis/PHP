0df72fbc4e00b585ab4840835613f5efa531785d
Snehashis Dalui <snehashisdalui087@gmail.com>
2026-09-01 10:19:22 +0530

    Convert string input to uppercase in HTML form

--- Assignment_2/4.php
+++ Assignment_2/4.php
@@ -1,11 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Convert Last 3 Characters</title>
+</head>
+<body>
 
-$str = readline("Enter a string: ");
+    <h2>Enter a String</h2>
 
-if (strlen($str) < 3) {
-    echo strtoupper($str);
-} else {
-    echo substr($str, 0, -3) . strtoupper(substr($str, -3));
-}
+    <form method="post">
+        <label>String:</label>
+        <input type="text" name="str" required>
+        <br><br>
 
-?>
+        <button type="submit">Convert</button>
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $str = $_POST["str"];
+
+        if (strlen($str) < 3) {
+            echo "<h3>Result: " . strtoupper($str) . "</h3>";
+        } else {
+            echo "<h3>Result: " .
+                substr($str, 0, -3) .
+                strtoupper(substr($str, -3)) .
+                "</h3>";
+        }
+    }
+    ?>
+
+</body>
+</html>
